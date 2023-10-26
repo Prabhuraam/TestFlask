29179a37a7fb661672376386f734ec50e60746c3
prabhu-16357 <prabhuraam775@gmail.com>
2023-10-26 11:04:44 +0530

    Test4

--- functions/Eventt/main.py
+++ functions/Eventt/main.py
@@ -4,6 +4,8 @@ import logging
 def handler(event, context):
     logger = logging.getLogger()
     logger.info('Hello from main.py')
+
+    '''Event Functionalities'''
     data = event.get_data()  # event data
     time = event.get_time()  # event occurred time
     action = event.get_action()
